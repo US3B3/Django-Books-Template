dc6d4c2badaf6bac7a8adb73dad55363ed85be2c
= <=>
2021-06-25 06:34:39 +0300

    admin arayüzünde listeleme, filtreleme ve arama kutusu özellikleri eklendi

--- benAnadolu_con/videolar/admin.py
+++ benAnadolu_con/videolar/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from . models import VideoDers
 
-admin.site.register(VideoDers)
+@admin.register(VideoDers)
+class VideoDersAdmin(admin.ModelAdmin):
+    list_display = ('isim','erisim', 'tarih',)
+    list_filter = ('erisim',)
+    search_fields = ('isim', 'aciklama')
