a82f4b081d8191ab615d3a6822f6cd931fa00968
= <=>
2021-07-11 18:54:04 +0300

    Önyüz tamamlandı ve tamplate klasörüne yerleştirildi

--- benAnadolu_con/sayfalar/urls.py
+++ benAnadolu_con/sayfalar/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('blog/', views.blog, name="blog"),
+    path('videoders/', views.videoders, name="videoders"),
+    path('galeri/', views.galeri, name="galeri"),
+    path('vitrin/', views.vitrin, name="vitrin"),
+    path('biyografi/', views.biyografi, name="biyografi"),
     path('hakkinda/', views.hakkinda, name="hakkinda"),
+    path('sayfa/', views.sayfa, name="sayfa"),
     path('dene/', views.dene, name="dene"),
 ]
--- benAnadolu_con/sayfalar/views.py
+++ benAnadolu_con/sayfalar/views.py
@@ -6,5 +6,23 @@ def index(request):
 def hakkinda(request):
     return render(request, "hakkinda.html")
 
+def blog(request):
+    return render(request, "blog.html")
+
+def videoders(request):
+    return render(request, "videoders.html")
+
+def galeri(request):
+    return render(request, "galeri.html")
+
+def vitrin(request):
+    return render(request, "vitrin.html")
+
+def biyografi(request):
+    return render(request, "biyografi.html")
+
+def sayfa(request):
+    return render(request, "sayfa.html")
+
 def dene(request):
     return render(request, "dene.html")
