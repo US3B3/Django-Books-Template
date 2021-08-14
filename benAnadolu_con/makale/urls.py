1a4cc052724dd1225994fa63d24a0ea48df6be38
= <=>
2021-08-15 01:24:23 +0300

    blog sayfasında sayfalama hariç her şey hazır

--- benAnadolu_con/makale/urls.py
+++ benAnadolu_con/makale/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.makale_list, name="blog"),
     path('kategori/', views.kategori_list, name="kategori_liste"),
+    path('kategori/<str:kategori_isim>', views.kategori_detay, name="kategori_detay"),
     path('<slug:kategori_slug>/<str:makale_isim>', views.makale_detay, name="makale_detay"),
 ]
--- benAnadolu_con/makale/views.py
+++ benAnadolu_con/makale/views.py
@@ -5,10 +5,12 @@ def makale_list(request):
     makaleler = Makale.objects.all().order_by('-tarih')
     basmakale = Makale.objects.all().filter(basMakale = 1).order_by('-tarih')
     talimakale = Makale.objects.all().filter(taliMakale = 1).order_by('-tarih')
+    kategoriler = Kategori.objects.order_by('isim')[0:11].all()
     context = {
         'makaleler' : makaleler,
         'basmakale' : basmakale[0],
-        'talimakale' : talimakale[0:2]
+        'talimakale' : talimakale[0:2],
+        'kategoriler' : kategoriler,
     }
     return render(request, "blog.html", context)
 
@@ -27,3 +29,14 @@ def kategori_list(request):
         'kategoriler' : kategoriler,
     }
     return render(request, "kategoriler.html", context)
+
+def kategori_detay(request, kategori_isim):
+    kategori_detay = Makale.objects.filter(kategori__isim=kategori_isim).all()
+    kategori = kategori_isim
+
+    context = {
+        'kategori_detay' : kategori_detay,
+        'kategori' : kategori,
+    }
+
+    return render(request, 'kategori.html', context)
