4134e5c8cbb11425a874974230afee76e3f5183c
= <=>
2021-09-07 02:06:56 +0300

    Siteyle işimiz bitti sıra deploymentta

--- benAnadolu_con/benAnadolu/settings.py
+++ benAnadolu_con/benAnadolu/settings.py
@@ -94,11 +94,15 @@ WSGI_APPLICATION = 'benAnadolu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'kokler',
+      'USER':'postgres',
+      'PASSWORD':'D33bl0e#',
+      'HOST':'localhost',
+      'PORT':'5432',
+   }
 }
 
 
